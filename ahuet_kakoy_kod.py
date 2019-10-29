8ba61a452db417b57c15305ca874fa0cd27e69b8
Igor Nurullaev <mighty.igor@yandex.ru>
2019-10-29 19:09:19 +0300

    Update ahuet_kakoy_kod.py

--- ahuet_kakoy_kod.py
+++ ahuet_kakoy_kod.py
@@ -3,7 +3,7 @@ import cv2
 import math
 
 lower = [0, 0, 0]
-higher = [50, 50, 50]
+higher = [60, 60, 60]
 lower_rgb = np.array(lower)
 higher_rgb = np.array(higher)
 size = width, height = 320, 240
@@ -11,6 +11,12 @@ center = width // 2, height // 2
 width_min, width_max = 100, 220
 height_min, height_max = 0, 150
 pi_na_dva = math.pi / 2
+
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
 
 
 def get_yaw(frame):
@@ -24,34 +30,29 @@ def get_yaw(frame):
         mask = cv2.inRange(frame[height_min:height_max, :], lower_rgb, higher_rgb)
         dx = 0
         max_y = 0
-        while mask[max_y].sum() == 0:
-            max_y += 1
+        try:
+            while mask[max_y].sum() == 0:
+                max_y += 1
+        except:
+            mask = cv2.inRange(frame, lower_rgb, higher_rgb)
+            try:
+                while mask[max_y].sum() == 0:
+                    max_y += 1
+            except:
+                return frame, None
 
     # cv2.imshow('mask', mask)
 
-    cv2.circle(frame, (dx + int(np.where(mask[max_y] == 255)[0].mean()), max_y), 6, (0, 255, 255), cv2.FILLED)
-    cv2.line(frame, center, (dx + int(np.where(mask[max_y] == 255)[0].mean()), max_y), (0, 255, 255), 3)
+    nearest_x = find_nearest(np.where(mask[max_y] == 255)[0], center[0])
 
     try:
-        yaw = (math.atan2(max_y - center[1],
-                          (dx + int(np.where(mask[max_y] == 255)[0].mean()) - center[0])) + pi_na_dva) % (2 * math.pi)
+        yaw = math.atan2(max_y - center[1], dx + nearest_x - center[0]) + pi_na_dva
     except:
         yaw = None
 
-    '''found = False
-    radius = 0
-    mask = cv2.inRange(frame, lower_rgb, higher_rgb)
-    while not found:
-        radius += 1
-        for _x in range(-radius, radius, 2):
-            _y = int((radius ** 2 - _x ** 2) ** 0.5)
-            if mask[center[1] - _y][center[0] + _x] != 0:
-                # cv2.line(frame, center, (center[0] + _x, center[1] - _y), (0, 255, 255))
-                # cv2.circle(frame, (center[0] + _x, center[1] - _y), 6, (0, 0, 255), cv2.FILLED)
-                cv2.line(frame, center, (center[0] + _x, center[1]), (255, 255, 0), 3)
-                found = True
-                radius = _x
-                break'''
+    cv2.circle(frame, (dx + nearest_x, max_y), 6, (0, 255, 255), cv2.FILLED)
+    cv2.line(frame, center, (dx + nearest_x, max_y), (0, 255, 255), 3)
+
     return frame, yaw  # , radius
 
 
