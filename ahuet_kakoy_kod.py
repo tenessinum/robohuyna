6f9a374d5f64e311c8fa66930b72905a23ff2e6f
Igor <mighty.igor@yandex.ru>
2019-11-06 17:15:41 +0300

    Немного математики

--- ahuet_kakoy_kod.py
+++ ahuet_kakoy_kod.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2
 import math
-from time import time
+from time import time, sleep
 
 lower = [0, 0, 0]
-higher = [40, 40, 40]
+higher = [50] * 3
 lower_rgb = np.array(lower)
 higher_rgb = np.array(higher)
 size = width, height = 320, 240
@@ -23,7 +23,7 @@ average_time_dict = {
 
 def get_velocity(_yaw):
     global max_velocity
-    return max_velocity * (1 - abs(_yaw) / math.pi)
+    return max_velocity * math.cos(_yaw)
 
 
 def find_nearest(array, value):
@@ -32,69 +32,69 @@ def find_nearest(array, value):
     return array[idx]
 
 
-def get_yaw(frame):
+def find_nearest_white(img, target):
+    nonzero = cv2.findNonZero(img)
+    distances = np.sqrt((nonzero[:, :, 0] - target[0]) ** 2 + (nonzero[:, :, 1] - target[1]) ** 2)
+    nearest_index = np.argmin(distances)
+    return nonzero[nearest_index]
+
+
+def get_yaw(frame, draw_rects=False, draw_contours=False, show_mask=False):
     global_mask = cv2.inRange(frame, lower_rgb, higher_rgb)
+
+    vertical_size = height // 30
+    vertical_structure = cv2.getStructuringElement(cv2.MORPH_RECT, (1, vertical_size))
+    global_mask = cv2.erode(global_mask, vertical_structure)
+    global_mask = cv2.dilate(global_mask, vertical_structure)
+
     mask = global_mask[height_min:height_max, width_min:width_max]
-    # contours, _ = cv2.findContours(global_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.drawContours(frame, contours, -1, (255, 150, 100), 2)
     dx = width_min
     max_y = 0
+
+    if mask.sum() == 0:
+        mask = global_mask[height_min:height_max, :]
+        dx = 0
+    if mask.sum() == 0:
+        mask = global_mask
 
     try:
         while mask[max_y].sum() == 0:
             max_y += 1
     except:
-        mask = global_mask[height_min:height_max, :]
-        dx = 0
-        max_y = 0
-        try:
-            while mask[max_y].sum() == 0:
-                max_y += 1
-        except:
-            mask = global_mask
-            try:
-                while mask[max_y].sum() == 0:
-                    max_y += 1
-            except:
-                return frame, None, None
+        return frame, None, None
 
-    # cv2.imshow('mask', global_mask)
     nearest_x = find_nearest(np.where(mask[max_y] == 255)[0], center[0])
 
     try:
         yaw = math.atan2(max_y - center[1], dx + nearest_x - center[0]) + pi_na_dva
+        if yaw > math.pi:
+            yaw -= math.pi * 2
     except:
         yaw = None
 
-    long_mask = global_mask[:, width_min:width_max]
-    y_i = 0
-    _dx = width_min
+    nearest_y = tuple(find_nearest_white(global_mask, center)[0])
 
-    while True:
-        try:
-            if long_mask[center[1] + y_i].sum() != 0:
-                break
-            elif long_mask[center[1] - y_i].sum() != 0:
-                y_i = -y_i
-                break
-            else:
-                y_i += 1
-        except:
-            long_mask = global_mask
-            y_i = 0
-            _dx = 0
+    if draw_contours or draw_rects:
+        contours, _ = cv2.findContours(global_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if draw_contours:
+            cv2.drawContours(frame, contours, -1, (0, 150, 255), 1)
+        if draw_rects:
+            for contour in contours:
+                rect = cv2.minAreaRect(contour)
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+                cv2.drawContours(frame, [box], 0, (0, 0, 255), 1)
 
-    x_i = find_nearest(np.where(long_mask[center[1] + y_i] == 255)[0], center[0])
-    cv2.circle(frame, (x_i + _dx, center[1] + y_i), 4, (0, 0, 255), cv2.FILLED)
-
-    cv2.circle(frame, (dx + nearest_x, max_y), 5, (0, 255, 255), cv2.FILLED)
-    cv2.line(frame, center, (dx + nearest_x, max_y), (0, 255, 255), 3)
-
-    return frame, yaw, (x_i + dx - center[0])
+    cv2.circle(frame, nearest_y, 3, (255, 0, 255), cv2.FILLED)
+    cv2.circle(frame, (dx + nearest_x, max_y), 4, (0, 255, 255), cv2.FILLED)
+    cv2.line(frame, center, (dx + nearest_x, max_y), (0, 255, 255), 2)
+    if show_mask:
+        cv2.imshow('mask', mask)
+    return frame, yaw, nearest_y[0] - center[0]
 
 
 if __name__ == '__main__':
-    cap = cv2.VideoCapture('assets/cool_vid.mp4')
+    cap = cv2.VideoCapture('assets/cool_vid2.mp4')
     out = cv2.VideoWriter('output/output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 60.0, (width, height))
     while cap.isOpened():
         ret, frame = cap.read()
@@ -110,11 +110,11 @@ if __name__ == '__main__':
             break
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        if cv2.waitKey(1) & 0xFF == ord('p'):
+            while cv2.waitKey(1) & 0xFF != ord('c'):
+                sleep(1)
     cap.release()
     out.release()
     cv2.destroyAllWindows()
-    print average_time_dict['t'] / average_time_dict['s']
-
-# 0.00111650835135 - time taken to execute get_yaw with drawing contours
-# 0.000837510282343 - time taken to execute get_yaw without drawing contours
-# 1.33312793274 times longer
+    print(average_time_dict['s'] / average_time_dict['t'], 'Hz')
+    print(average_time_dict['t'] / average_time_dict['s'], 'SPF')
